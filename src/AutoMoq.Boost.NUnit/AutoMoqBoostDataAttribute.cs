a70bcee88ba47e6ffef529e57f186ad63c902463
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-08-02 16:58:02 +0100

    Added Boost.NUnit plugin

---
+++ src/AutoMoq.Boost.NUnit/AutoMoqBoostDataAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.NUnit2;
+
+namespace Dash.AutoMoq.Boost.NUnit
+{
+    public class AutoMoqBoostDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqBoostDataAttribute()
+            : base(new Fixture()
+                       .Customize(new AutoMoqBoostCustomization()))
+        {
+        }
+    }
+}
