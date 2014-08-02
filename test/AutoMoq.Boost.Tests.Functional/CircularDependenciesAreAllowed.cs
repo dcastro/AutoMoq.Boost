9157fd60e7d71b5db7f4f095233c793e75503286
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-08-02 14:57:57 +0100

    Added functional tests

---
+++ test/AutoMoq.Boost.Tests.Functional/CircularDependenciesAreAllowed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dash.AutoMoq.Boost.xUnit;
+using Ploeh.AutoFixture;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Dash.AutoMoq.Boost.Tests.Functional
+{
+    public class CircularDependenciesAreAllowed
+    {
+        [Theory, AutoMoqBoostData]
+        public void Test(Fixture fixture)
+        {
+            Assert.DoesNotThrow(() => fixture.Freeze<IDbConnection>());
+        }
+
+        public interface IDbConnection
+        {
+            IDbConnection Clone();
+        }
+    }
+}
