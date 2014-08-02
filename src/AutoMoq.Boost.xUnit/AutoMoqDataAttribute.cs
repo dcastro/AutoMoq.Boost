ef23348aacc833957cfc726aa9df37d535aa39b3
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-08-02 13:02:08 +0100

    Added Boost.xUnit plugin

---
+++ src/AutoMoq.Boost.xUnit/AutoMoqDataAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dash.AutoMoq.Boost;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Xunit;
+
+namespace Dash.AutoMoq.Boost.xUnit
+{
+    public class AutoMoqBoostDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqBoostDataAttribute()
+            : base(new Fixture()
+                       .Customize(new AutoMoqBoostCustomization()))
+        {
+        }
+    }
+}
